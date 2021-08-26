6420ceb45c3f2c7817f64513766d5de8873197ac
Thanthu <tnair@orthofx.com>
2021-08-26 17:30:53 +0530

    Initial project commit

---
+++ src/main/java/com/thanthu/webfluxmongoreactive/WebfluxMongoReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.thanthu.webfluxmongoreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxMongoReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxMongoReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanthu/webfluxmongoreactive/WebfluxMongoReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanthu.webfluxmongoreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxMongoReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
