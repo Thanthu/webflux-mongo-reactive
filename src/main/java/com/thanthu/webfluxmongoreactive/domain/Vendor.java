457cc8750c0d579c70b05561a3e3e91a777655ad
Thanthu <tnair@orthofx.com>
2021-08-26 20:15:27 +0530

    Bootstrap with Vendor and Category data. Closes #3.

--- src/main/java/com/thanthu/webfluxmongoreactive/domain/Vendor.java
+++ src/main/java/com/thanthu/webfluxmongoreactive/domain/Vendor.java
@@ -3,18 +3,24 @@ package com.thanthu.webfluxmongoreactive.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Document
 public class Vendor {
 	
 	@Id
 	private String id;
 	
-	private String firstname;
-	private String lastname;
+	private String firstName;
+	private String lastName;
 
 }
