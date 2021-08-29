615410877f61c9ab66e766ae9da9179b91ca24e4
Thanthu <tnair@orthofx.com>
2021-08-29 13:36:18 +0530

    Add API to update Category. Closes #7.

--- src/main/java/com/thanthu/webfluxmongoreactive/controllers/CategoryController.java
+++ src/main/java/com/thanthu/webfluxmongoreactive/controllers/CategoryController.java
@@ -1,10 +1,10 @@
 package com.thanthu.webfluxmongoreactive.controllers;
 
-import org.reactivestreams.Publisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,8 +36,14 @@ public class CategoryController {
 
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/api/v1/categories")
-	Mono<Void> create(@RequestBody Mono<Category> categoryStream) { //  Use  Mono<Category> to acceptl list and create multiple
+	Mono<Void> create(@RequestBody Mono<Category> categoryStream) { // Use Mono<Category> to acceptl list and create multiple
 		return categoryRepository.saveAll(categoryStream).then();
+	}
+
+	@PutMapping("/api/v1/categories/{id}")
+	Mono<Category> update(@PathVariable String id, @RequestBody Category category) {
+		category.setId(id);
+		return categoryRepository.save(category);
 	}
 
 }
--- src/test/java/com/thanthu/webfluxmongoreactive/controllers/CategoryControllerTest.java
+++ src/test/java/com/thanthu/webfluxmongoreactive/controllers/CategoryControllerTest.java
@@ -62,4 +62,15 @@ class CategoryControllerTest {
 				.isCreated();
 	}
 
+	@Test
+	public void testUpdateCateogry() {
+		BDDMockito.given(categoryRepository.save(any(Category.class)))
+				.willReturn(Mono.just(Category.builder().build()));
+
+		Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Some Cat").build());
+
+		webTestClient.put().uri("/api/v1/categories/asdfasdf").body(catToUpdateMono, Category.class).exchange()
+				.expectStatus().isOk();
+	}
+
 }
